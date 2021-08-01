19c31052507090b83a5537b6f3d5085deca38a5a
Евгений <festProbe@gmail.com>
2021-08-02 04:11:54 +1100

    Добавил контейнеры для карточек фильмов

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import { createFilmCardTemplate } from './view/film-card';
+import { createFilmListTemplate } from './view/film-container';
 import { createFilmsCountTemplate } from './view/films-count-stat';
 import { createProfileRatingTemplate } from './view/header';
-import { createNavigationMenuTemplate } from './view/navigation-sort-menu';
+import { createNavigationMenuTemplate } from './view/navigation-menu';
 //import { createPopupTemplate } from './view/popup';
 import { createShowMoreButtonTemplate } from './view/show-more-btn';
 import { createSortMenuTemplate } from './view/sort-menu';
+import { createMostCommentedFilmsListTemplate } from './view/most-commented-films-container';
+import { createTopRatedFilmsListTemplate } from './view/top-rated-films-container';
 
 const FILMS_COUNT = 5;
-const EXTRA_FILMS_COUNT = 2;
+const TOP_RATED_FILMS_COUNT = 2;
+const MOST_COMMENTED_FILMS_COUNT = 2;
 
 
 const renderPage = (container, template, place) => {
@@ -18,28 +22,31 @@ const siteHeaderElement = document.querySelector('.header');
 renderPage(siteHeaderElement, createProfileRatingTemplate(), 'beforeend');
 
 const siteMainElement = document.querySelector('.main');
-const navMenuElement = siteMainElement.querySelector('.main-navigation');
-renderPage(navMenuElement, createNavigationMenuTemplate(), 'beforeend');
-const sortMenuElement = siteMainElement.querySelector('.sort');
-renderPage(sortMenuElement, createSortMenuTemplate(), 'beforeend');
+renderPage(siteMainElement, createNavigationMenuTemplate(), 'beforeend');
+renderPage(siteMainElement, createSortMenuTemplate(), 'beforeend');
 
 
+renderPage(siteMainElement, createFilmListTemplate(), 'beforeend');
 const filmCardContainer = document.querySelector('.films-list__container');
 for (let i = 0; i < FILMS_COUNT; i++) {
   renderPage(filmCardContainer, createFilmCardTemplate(), 'beforeend');
 }
 
-const extraFilmsElements = document.querySelectorAll('.films-list--extra > .films-list__container');
-for (const element of extraFilmsElements) {
-  for (let i = 0; i < EXTRA_FILMS_COUNT; i++) {
-    renderPage(element, createFilmCardTemplate(), 'beforeend');
-  }
+renderPage(siteMainElement, createTopRatedFilmsListTemplate(), 'beforeend');
+const topRatedFilmsElement = document.querySelector('.films-list--top-rated > .films-list__container');
+for (let i = 0; i < TOP_RATED_FILMS_COUNT; i++) {
+  renderPage(topRatedFilmsElement, createFilmCardTemplate(), 'beforeend');
 }
+
+renderPage(siteMainElement, createMostCommentedFilmsListTemplate(), 'beforeend');
+const mostCommentedFilmsElement = document.querySelector('.films-list--most-commented > .films-list__container');
+for (let i = 0; i < MOST_COMMENTED_FILMS_COUNT; i++) {
+  renderPage(mostCommentedFilmsElement, createFilmCardTemplate(), 'beforeend');
+}
+
 renderPage(siteMainElement, createShowMoreButtonTemplate(), 'beforeend');
-//renderPage(siteMainElement, createMainPage(), 'beforeend');
 
 const siteFooterStatisticElement = document.querySelector('.footer__statistics');
 renderPage(siteFooterStatisticElement, createFilmsCountTemplate(), 'beforeend');
 
-//const filmPopupElement = document.querySelector('.film-details');
-//renderPage(filmPopupElement, createPopupTemplate(), 'beforeend');
+//renderPage(siteMainElement, createPopupTemplate(), 'beforeend');
